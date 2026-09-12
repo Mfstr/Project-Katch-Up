0d516bc88fa484ba030ac1e115566b90c018b04d
Michael Foster <mafoster060@gmail.com>
2026-09-12 14:06:07 -0500

    feat: add initial database schema with profiles, tasks, and calendar tables

---
+++ supabase/schema.sql
@@ -0,0 +1,33 @@
+-- WARNING: This schema is for context only and is not meant to be run.
+-- Table order and constraints may not be valid for execution.
+
+CREATE TABLE public.profiles (
+  id uuid NOT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  ical_url text UNIQUE,
+  focus_interval integer,
+  break_interval integer,
+  pomodoro_cycles integer,
+  CONSTRAINT profiles_pkey PRIMARY KEY (id),
+  CONSTRAINT profiles_id_fkey FOREIGN KEY (id) REFERENCES auth.users(id),
+  CONSTRAINT profiles_ical_url_fkey FOREIGN KEY (ical_url) REFERENCES public.calendar(ical_url)
+);
+CREATE TABLE public.tasks (
+  id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  title text,
+  description text,
+  is_complete boolean,
+  due_date time without time zone,
+  profile_id uuid UNIQUE,
+  CONSTRAINT tasks_pkey PRIMARY KEY (id),
+  CONSTRAINT tasks_profile_id_fkey FOREIGN KEY (profile_id) REFERENCES public.profiles(id)
+);
+CREATE TABLE public.calendar (
+  id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  name text,
+  description text,
+  ical_url text UNIQUE,
+  CONSTRAINT calendar_pkey PRIMARY KEY (id)
+);
